d57b89a84e7571326478d34e63446b1d5e93c7ca
Pedro Henrique Wanot <pedrowanotchikoski@gmail.com>
2026-08-13 19:39:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -227,7 +227,7 @@
 
   $("#openInvite")?.addEventListener("click", () => {
     envelope?.classList.add("open");
-    setTimeout(() => intro?.classList.add("opened"), 5000);
+    setTimeout(() => intro?.classList.add("opened"), 8000);
   });
 
   // ---------------------------------------------------------------------------
