251b5efe063656093882766548d859a8a2fc52b0
Pedro Henrique Wanot <pedrowanotchikoski@gmail.com>
2026-08-13 19:36:02 -0300

    Increase timeout for intro opening animation

--- app.js
+++ app.js
@@ -227,7 +227,7 @@
 
   $("#openInvite")?.addEventListener("click", () => {
     envelope?.classList.add("open");
-    setTimeout(() => intro?.classList.add("opened"), 3000);
+    setTimeout(() => intro?.classList.add("opened"), 5000);
   });
 
   // ---------------------------------------------------------------------------
